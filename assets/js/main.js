3e09b20eadbe7f196c9e823ea28a42abb7f156bf
malauryn <malonelauryn014@gmail.com>
2026-02-10 05:35:27 +0000

    Refactor styles and update HTML structure for improved layout and typography

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 // main.js
+let targetElement = document.querySelector("h2");
 
+conesole.log(targetElement.outerHTML);
+
+targetElement.addEventListener("mouseover", function () {
+    console.log(this.innerText);
+    this.innerText = "TIME";
+    this.style = "cursor: wait";
+});
+
+targetElement.addEventListener("mouseout", function () {
+    this.innerText = "IKEA";
+    this.style = "cursor: cursor pointer";
+}
 console.log("Hello, World!");
